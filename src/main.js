fd0a492dd380d407cbbd745b03eb677bf63d4e5e
zej <zej15158802043>
2020-02-18 13:05:32 +0800

    代码优化

--- src/main.js
+++ src/main.js
@@ -9,17 +9,17 @@ import 'ant-design-vue/dist/antd.css'
 Vue.use(Antd);
 
 // 引入颜色选择器插件
-import vcolorpicker from 'vcolorpicker'
-Vue.use(vcolorpicker)
+// import vcolorpicker from 'vcolorpicker'
+// Vue.use(vcolorpicker)
 
 // 引入common.css
 import './assets/css/common.css';
 // global.css
-import './assets/css/global.css';
+import './assets/css/global.less';
 // 引入base.css
 import './assets/css/base.less';
 // 引入layout.css（响应式布局）
-import './assets/css/layout.css';
+import './assets/css/layout.less';
 
 Vue.config.productionTip = false
 
